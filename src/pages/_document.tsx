c2a12b4a7fe4f205ed9e5f76171896e45cf31a96
Walter Alcantara <waltermalcantara@gmail.com>
2022-02-19 13:25:02 -0300

    feat(meta): add meta informations for social media

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,6 +42,25 @@ export default class MyDocument extends Document {
         <Head>
           <style>{`#__next { overflow-x: hidden; }`}</style>
 
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <meta name="title" content="Walter Alcantara" />
+          <meta name="description" content="Walter's Homepage" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://walteralcantara.vercel.app/" />
+          <meta property="og:title" content="Walter Alcantara" />
+          <meta property="og:description" content="Walter's Homepage" />
+          <meta property="og:image" content="https://walteralcantara.vercel.app/card.png" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://walteralcantara.vercel.app/" />
+          <meta property="twitter:title" content="Walter Alcantara" />
+          <meta property="twitter:description" content="Walter's Homepage" />
+          <meta property="twitter:image" content="https://walteralcantara.vercel.app/card.png" />
+
+          <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+          <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
